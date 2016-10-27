7da8713c17c879d8a2edaa31b032a727027d0ed9
V <v@Chappie.local>
2016-10-27 19:49:03 +0300

    Shit

--- dev/js/020_frontend-header.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function(){
-    var fromtop = $(document).scrollTop();
-    opac = fromtop/200;
-    if(opac > 0.96){
-    	opac = 0.96;
-    }
-    $('.header-bcg').css({opacity: opac});
-    console.log(opac);
-});
--- opencart/drop_opencart-upload_here.md
+++
@@ -1,1 +0,0 @@
-#Drop opencart-upload here :)
