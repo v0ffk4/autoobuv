2fd409fffca7e1f1f561a29ccefc27e6ca2bdf50
V <v@Chappie.local>
2016-11-07 20:27:25 +0300

    Quicksave

--- dev/js/030_frontendCategorySubmenu.js
+++ dev/js/030_frontendCategorySubmenu.js
@@ -3,13 +3,17 @@ function categorySubmenu() {
 	//задаем маркер по-умолчанию
 	var currentMenuMarker = '-1';
 
+	//handles for GSAP
+	var navUl = $('.sublevel-02 ul');
+	var navLi = $('.sublevel-02 li');
+
 	$('.sublevel-01 li').click(function(){
 
 		//если кликнутый пункт равен текущему, то закрываем нафик субменю
 		if( $(this).index() == currentMenuMarker ) {
 
 			//закрываем
-			$('.sublevel-02 ul').removeClass("active");
+			$(navUl).removeClass("active");
 
 			//сбрасываем маркер
 			currentMenuMarker = -1;
@@ -19,10 +23,19 @@ function categorySubmenu() {
 			//меняем маркер на кликнутый
 			currentMenuMarker = $(this).index();
 			//ловим нужный подпункт
-			var target = $('.sublevel-02 ul')[currentMenuMarker];
+			var currentMenu = $('.sublevel-02 ul')[currentMenuMarker];
 
 			$('.sublevel-02 ul').removeClass("active");
-			$(target).addClass("active");
+			$(currentMenu).addClass("active");
+			var currentMenuListItems = $('.active li');
+
+			//GSAP
+
+			tl = new TimelineMax();
+
+			tl
+				.to( currentMenuListItems, 0.25, { opacity: 0 } )
+				.to( currentMenuListItems, 0.5, { opacity: 1 } );
 
 
 		}
