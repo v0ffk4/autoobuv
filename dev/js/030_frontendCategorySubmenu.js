f3b0a592fd180d578b00348fc7490d9048490789
V <v@Chappie.local>
2016-11-08 14:59:54 +0300

    Quicksave

--- dev/js/030_frontendCategorySubmenu.js
+++ dev/js/030_frontendCategorySubmenu.js
@@ -8,50 +8,25 @@ function categorySubmenu() {
 	var navUl = $('.sublevel-02 ul');
 	var navLi = $('.sublevel-02 li');
 
+	//show / hide
+	function showNavbar() {
+			
+		$(navBar).addClass('active');
+		TweenLite.to(navBar, 1, {opacity: 1});
+
+	}
+
+	function showNavUl(current) {
+
+
+		$(navUl)[current].show();
+
+	}
+
+
 	$('.sublevel-01 li').click(function(){
 
-		//если кликнутый пункт равен текущему, то закрываем нафик субменю
-		if( $(this).index() == currentMenuMarker ) {
-
-			//закрываем
-			$(navUl).removeClass("active");
-
-			//
-			tlHide = new TimelineMax();
-
-			tlHide
-				.to( navLi, 0.25, { opacity: 0 } )
-
-			//сбрасываем маркер
-			currentMenuMarker = -1;
-
-		} else {
-
-			//меняем маркер на кликнутый
-			currentMenuMarker = $(this).index();
-
-			//ловим нужный подпункт
-			var currentUl = $('.sublevel-02 ul')[currentMenuMarker];
-
-			//активируем класс
-			$(navUl).removeClass("active");
-			$(currentUl).addClass("active");
-
-			var currentLi = $('.active li');
-
-
-			//GSAP
-
-			tlShow = new TimelineMax();
-
-			tlShow
-				.to( currentLi, 0, { opacity: 0 } )
-				.to( currentLi, 0, { x: '+=25' } )
-				.to( currentLi, 0.75, { x: '-=25' } )
-				.to( currentLi, 0.75, { opacity: 1 }, -0.75 );
-
-		}
-
+		showNavbar();
 		
 	}); 
 	//end onclick
